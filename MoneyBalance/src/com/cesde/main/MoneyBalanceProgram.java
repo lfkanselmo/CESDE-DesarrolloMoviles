f6047f9c29101371dc6f01823aea13429aea8fc4
Anselmo Ortega <lfkanselmo@gmail.com>
2023-08-29 16:57:37 -0500

    Update MoneyBalanceProgram.java

--- MoneyBalance/src/com/cesde/main/MoneyBalanceProgram.java
+++ MoneyBalance/src/com/cesde/main/MoneyBalanceProgram.java
@@ -1,4 +1,12 @@
 package com.cesde.main;
 
 public class MoneyBalanceProgram {
+    public static void main(String[] args) {
+        start();
+    }
+
+    //Metodo para iniciar el programa
+    public static void start(){
+        System.out.println("*************** BIEVENIDO ***************");
+    }
 }
